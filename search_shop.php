8dd24fb8b8b025e5294b1024e02f2766db4d2d00
austin030606 <austin030606@gmail.com>
2022-05-19 18:47:21 +0800

    fix: fix distance value 0 bug

--- search_shop.php
+++ search_shop.php
@@ -1,5 +1,6 @@
 <?php
     function distance($lat1, $lon1, $lat2, $lon2, $unit) {
+        if ($lat1 == $lat2 && $lon1 == $lon2) return 0.0001;
         $theta = $lon1 - $lon2;
         $dist = sin(deg2rad($lat1)) * sin(deg2rad($lat2)) +  cos(deg2rad($lat1)) * cos(deg2rad($lat2)) * cos(deg2rad($theta));
         $dist = acos($dist);

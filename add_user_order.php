d6a91ed0fe37aea8680963cfa99cc8d385453a52
detaomega <cosmolynnyang@gmail.com>
2022-06-23 01:52:15 +0800

    feat: user can order item and successful insert in database

--- add_user_order.php
+++ add_user_order.php
@@ -47,17 +47,52 @@
                 "distance" => $distance
             )
         );
-        echo "<script>alert(\"$distance $stTime $total $mode $SID $UID $distance\"); window.location.replace(\"nav.php\");</script>";    
-
-        exit();
+        $OID = $conn->lastInsertId();
+    
+        // user wallet 
+        $stmt = $conn -> prepare("select wallet from user where ID=:ID");
+        $stmt -> execute(array("ID" => $UID));
+        $userInfo = $stmt -> fetch();
+        $newValue = $userInfo["wallet"] - $total;  
+        $stmt = $conn -> prepare("update user set wallet =:value where user.ID=:ID");
+        $stmt -> execute(array("value" => $newValue, "ID" => $UID));
+           
+        // add contains from OID and PID
+        $stmt = $conn -> prepare("select * from product where SID=:SID");
+        $stmt -> execute(array("SID" => $SID));
+        $result = $stmt -> fetchAll();
+        foreach ($result as &$row) {
+            $ID = $row['ID'];
+            $orderQuantity = $_POST["$ID"];
+            if ($orderQuantity == 0) {
+                continue;
+            }
+            $stmt = $conn -> prepare (
+                "insert into `contains` (
+                    OID,
+                    PID,
+                    number
+                ) 
+                values (
+                    :OID,  
+                    :PID, 
+                    :number 
+                )"
+            );
+            $stmt->execute(
+                array (
+                    "OID" => $OID,
+                    "PID" => $ID, 
+                    "number" => $orderQuantity
+                )
+            );
+        }         
+        echo "<script>alert(\"success!!\"); window.location.replace(\"nav.php\");</script>";
     } 
     catch (Exception $e) {
         $msg = $e->getMessage();
         echo "<script>alert(\"$msg\"); window.location.replace(\"nav.php\");</script>";
     }
-    // $stmt = $conn -> prepare("select * from product where SID=:SID");
-    // $stmt -> execute(array("SID" => $SID));
-    // $result = $stmt -> fetchAll();
-  
+
 
 ?>

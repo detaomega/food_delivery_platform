71bc36028d6eb37af174d8aaa56f0daa8170925f
austin030606 <austin030606@gmail.com>
2022-06-23 14:34:59 +0800

    feat: now creates a transaction record

--- edit_wallet.php
+++ edit_wallet.php
@@ -18,20 +18,25 @@
         }
 
         $value = $_POST["value"];
+        $stTime = date("Y-m-d H:i:s");
         if (!preg_match('/^[0-9]+$/', $_POST["value"])) {
             throw new Exception("Please make sure the value is a positive integer.");
         }
         
-        $stmt=$conn->prepare("select wallet from user where account=:account");
+        $stmt=$conn->prepare("select wallet, ID from user where account=:account");
         $stmt->execute(array("account" => $_SESSION["account"]));
         $row = $stmt->fetch();
         $value = $value + $row["wallet"];
+        $UID = $row["ID"];
         if ($value < 0) {
             throw new Exception("Not enough money.");
         }
 
         $stmt = $conn->prepare("update user set wallet = :value where user.account = :account");
         $stmt->execute(array("value" => $value, "account" => $_SESSION["account"]));
+
+        $stmt = $conn->prepare("insert into `transaction` (`type`, `price`, `time`, `UID`) VALUES (:type, :price, :time, :UID)");
+        $stmt->execute(array("type" => "Recharge", "price" => $_POST["value"], "time" => $stTime, "UID" => $UID));
         echo json_encode(array("error" => false, "text" => "success"));
     } catch (Exception $e) {
         $msg = $e->getMessage();

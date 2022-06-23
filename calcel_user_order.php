d7febaf9342a604e7f4efaade8878533834f0093
austin030606 <austin030606@gmail.com>
2022-06-23 21:45:16 +0800

    feat: add cancel order feature

--- calcel_user_order.php
+++ calcel_user_order.php
@@ -4,6 +4,7 @@
     $dbname = "DB_HW";  
     $dbusername = "dev"; 
     $dbpassword = "devpasswd";
+    date_default_timezone_set("Asia/Taipei");
 
     if (!isset($_SESSION['Authenticated']) || $_SESSION['Authenticated'] != true) {
         header("Location: index.php");
@@ -15,9 +16,61 @@
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     try { 
-        $ID = $_POST["OID"];
-        $stmt = $conn -> prepare("update `order` set status = :newStatus where ID = :OID");
-        $stmt -> execute(array("newStatus" => "Cancel", "OID" => $ID));
+        $OID = $_POST["OID"];
+        $stTime = date("Y-m-d H:i:s");
+        $stmt = $conn -> prepare("select * from `order` where ID=:OID");
+        $stmt -> execute(array("OID" => $OID));
+        $row = $stmt -> fetch();
+        if ($row["status"] != "Not finish") {
+            throw new Exception("The order is already finished or canceled.");
+        }
+        $total = $row["payment"];
+        $SID = $row["SID"];
+        $UID = $row["UID"];
+        $stmt=$conn->prepare("select UID from store where ID=:SID");
+        $stmt->execute(array("SID" => $SID));
+        $row = $stmt->fetch();
+        $SUID = $row["UID"];
+
+        //products
+        $stmt = $conn -> prepare("select * from `contains` where OID=:OID");
+        $stmt -> execute(array("OID" => $OID));
+        while ($row = $stmt -> fetch()) {
+            $PID = $row["PID"];
+            $productStmt = $conn -> prepare("select * from `product` where ID=:PID");
+            $productStmt -> execute(array("PID" => $PID));
+            if ($productStmt -> rowCount() == 0) continue;
+            $productInfo = $productStmt -> fetch();
+            $newQuantity = $productInfo["quantity"] + $row["number"];
+            $productStmt = $conn->prepare("update `product` set quantity = :quantity where ID = :PID");
+            $productStmt->execute(array("quantity" => $newQuantity, "PID" => $PID));
+        }
+
+        //wallet
+        $stmt = $conn -> prepare("select wallet from user where ID=:ID");
+        $stmt -> execute(array("ID" => $UID));
+        $userInfo = $stmt -> fetch();
+        $newValue = $userInfo["wallet"] + $total;  
+        $stmt = $conn -> prepare("update user set wallet =:value where user.ID=:ID");
+        $stmt -> execute(array("value" => $newValue, "ID" => $UID));
+
+        $stmt = $conn -> prepare("select wallet from user where ID=:ID");
+        $stmt -> execute(array("ID" => $SUID));
+        $userInfo = $stmt -> fetch();
+        $newValue = $userInfo["wallet"] - $total; 
+        $stmt = $conn -> prepare("update user set wallet =:value where user.ID=:ID");
+        $stmt -> execute(array("value" => $newValue, "ID" => $SUID));
+
+        //transaction
+        $stmt = $conn->prepare("insert into `transaction` (`type`, `price`, `time`, `UID`, `target_UID`, `is_refund`) VALUES (:type, :price, :time, :UID, :target_UID, :is_refund)");
+        $stmt->execute(array("type" => "Takings", "price" => $total, "time" => $stTime, "UID" => $UID, "target_UID" => $SUID, "is_refund" => 1));
+
+        $stmt = $conn->prepare("insert into `transaction` (`type`, `price`, `time`, `UID`, `target_UID`, `is_refund`) VALUES (:type, :price, :time, :UID, :target_UID, :is_refund)");
+        $stmt->execute(array("type" => "Payment", "price" => $total, "time" => $stTime, "UID" => $SUID, "target_UID" => $UID, "is_refund" => 1));
+
+        //order status, time
+        $stmt = $conn->prepare("update `order` set status = :status, finish_time = :time where ID = :OID");
+        $stmt->execute(array("status" => "Cancel", "time" => $stTime, "OID" => $OID));
         echo "<script>alert(\"You have cancel your order!!\"); window.location.replace(\"nav.php#menu1\");</script>";
     } 
     catch (Exception $e) {
